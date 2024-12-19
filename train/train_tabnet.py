4a8472b0e15530cf06f7c22dd29df8f19c905a24
yattinda <yattinda@gmail.com>
2024-12-19 13:44:57 +0900

    Model at tabnet

--- train/train_tabnet.py
+++ train/train_tabnet.py
@@ -31,15 +31,15 @@ data = data.dropna()
 
 # 特徴量とターゲットの分割
 train, test = split_date(data, 0.2)
-X_train = train.drop(['着順','オッズ','人気','上がり','走破時間','通過順'], axis=1).values
-y_train = train['着順'].values
-X_test = test.drop(['着順','オッズ','人気','上がり','走破時間','通過順'], axis=1).values
-y_test = test['着順'].values
+X_train = train.drop(['着順','オッズ','人気','上がり','走破時間','通過順'], axis=1)
+y_train = train['着順']
+X_test = test.drop(['着順','オッズ','人気','上がり','走破時間','通過順'], axis=1)
+y_test = test['着順']
 
-X_train = torch.tensor(X_train).to(device)
-y_train = torch.tensor(y_train).to(device)
-X_test = torch.tensor(X_test).to(device)
-y_test = torch.tensor(y_test).to(device)
+X_train = torch.tensor(X_train.values).to(device)
+y_train = torch.tensor(y_train.values).to(device)
+X_test = torch.tensor(X_test.values).to(device)
+y_test = torch.tensor(y_test.values).to(device)
 
 class_weights = list(class_weight.compute_class_weight('balanced',
                     classes=np.unique(y_train.cpu()),
@@ -56,8 +56,8 @@ cross_entropy_loss_wight = CrossEntropyLoss(weight=weights)
 unsupervised_model = TabNetPretrainer(
     n_d=64,
     n_a=64,
-    n_steps=10,
-    gamma=2,
+    n_steps=4,
+    gamma=1.4,
     lambda_sparse=1e-5,
     optimizer_fn=torch.optim.Adam,
     optimizer_params=dict(lr=2e-2, weight_decay=1e-5),
@@ -71,8 +71,8 @@ print('pretraining...')
 unsupervised_model.fit(
     X_train=X_train.cpu(),
     eval_set=[X_test.cpu()],
-    batch_size=2048,
-    virtual_batch_size=128,
+    batch_size=4096,
+    virtual_batch_size=256,
     num_workers=0,
     drop_last=False,
     pretraining_ratio=0.5,
@@ -85,8 +85,8 @@ loaded_pretrain.load_model('/work/yasuhito-m/workspace/keiba-ai/model/tabnet/pre
 params = {
     'n_d': 64,
     'n_a': 64,
-    'n_steps': 10,
-    'gamma': 2,
+    'n_steps': 4,
+    'gamma': 1.4,
     'lambda_sparse': 1e-5,
     'optimizer_fn': torch.optim.Adam,
     'optimizer_params': dict(lr=2e-2, weight_decay=1e-5),
@@ -100,19 +100,20 @@ params = {
 
 X_train_cpu = X_train.cpu().numpy()
 X_test_cpu = X_test.cpu().numpy()
-
+y_train_cpu = y_train.cpu().numpy()
+y_test_cpu = y_test.cpu().numpy()
 print('training...')
 tabnet_clf = TabNetClassifier(**params)
 
 tabnet_clf.fit(
     X_train=X_train_cpu,
-    y_train=y_train.cpu().numpy(),
+    y_train=y_train_cpu,
     loss_fn = cross_entropy_loss_wight,
-    eval_set=[(X_test_cpu, y_test.cpu().numpy())],
+    eval_set=[(X_test_cpu, y_test_cpu)],
     eval_name=['test'],
     eval_metric=['balanced_accuracy'],
     patience=20,
-    batch_size=1024,
+    batch_size=4096,
     virtual_batch_size=128,
     num_workers=0,
     drop_last=False,
@@ -122,17 +123,17 @@ tabnet_clf.fit(
 joblib.dump(tabnet_clf,'/work/yasuhito-m/workspace/keiba-ai/model/tabnet/tabnet.pkl')
 #joblib.dump(lgb_clf,'model/lightgbm/lightgbm.pkl')
 
-y_pred_train = tabnet_clf.predict_proba(X_train_cpu.values)[:,1]
-y_pred = tabnet_clf.predict_proba(X_test_cpu.values)[:,1]
+y_pred_train = tabnet_clf.predict_proba(X_train_cpu)[:,1]
+y_pred = tabnet_clf.predict_proba(X_test_cpu)[:,1]
 
 #モデルの評価
 #print(roc_auc_score(y_train,y_pred_train))
-print(roc_auc_score(y_test,y_pred))
+print(roc_auc_score(y_test_cpu, y_pred))
 total_cases = len(y_test)  # テストデータの総数
-TP = (y_test == 1) & (y_pred >= 0.5)  # True positives
-FP = (y_test == 0) & (y_pred >= 0.5)  # False positives
-TN = (y_test == 0) & (y_pred < 0.5)  # True negatives
-FN = (y_test == 1) & (y_pred < 0.5)  # False negatives
+TP = (y_test_cpu == 1) & (y_pred >= 0.5)  # True positives
+FP = (y_test_cpu == 0) & (y_pred >= 0.5)  # False positives
+TN = (y_test_cpu == 0) & (y_pred < 0.5)  # True negatives
+FN = (y_test_cpu == 1) & (y_pred < 0.5)  # False negatives
 
 TP_count = sum(TP)
 FP_count = sum(FP)
@@ -154,11 +155,12 @@ print("False negatives:", FN_count, "(", "{:.2f}".format(misclassification_rate_
 importance = tabnet_clf.feature_importances_
 
 # 特徴量の名前を取得
-feature_names = X_train.columns
+feature_data = train.drop(['着順','オッズ','人気','上がり','走破時間','通過順'], axis=1)
+feature_names = feature_data.columns
 
 # 特徴量の重要度を降順にソート
 indices = np.argsort(importance)[::-1]
 
 # 特徴量の重要度を降順に表示
-for f in range(X_train.shape[1]):
+for f in range(feature_data.shape[1]):
     print("%2d) %-*s %f" % (f + 1, 30, feature_names[indices[f]], importance[indices[f]]))
